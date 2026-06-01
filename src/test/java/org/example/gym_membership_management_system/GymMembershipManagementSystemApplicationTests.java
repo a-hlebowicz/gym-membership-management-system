62888c500938659bdf51315b817c336793592267
a-hlebowicz <aleksanderhlebowicz@gmail.com>
2026-06-01 16:33:12 +0200

    initialize project via Spring Initializr

---
+++ src/test/java/org/example/gym_membership_management_system/GymMembershipManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gym_membership_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymMembershipManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
