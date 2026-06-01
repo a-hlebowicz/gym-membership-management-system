7dc78e63986b4015886dd2137eb7d528dc347e5d
a-hlebowicz <aleksanderhlebowicz@gmail.com>
2026-06-01 20:24:01 +0200

    add global exception handler

---
+++ src/main/java/org/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package org.example.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    //conflict = 409; when ConflictException go here
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<Map<String, String >> handleConflict(ConflictException exception){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error", exception.getMessage()));
+    }
+}
