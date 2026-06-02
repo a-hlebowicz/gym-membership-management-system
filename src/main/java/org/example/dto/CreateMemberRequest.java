a4cc231a6350fa1dd9da7f326d68ea2980b1a5d4
a-hlebowicz <aleksanderhlebowicz@gmail.com>
2026-06-03 00:56:50 +0200

    add member repository and create request dto

---
+++ src/main/java/org/example/dto/CreateMemberRequest.java
@@ -0,0 +1,25 @@
+package org.example.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateMemberRequest {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String surname;
+
+    @NotBlank
+    //@Email //TODO easier tests; remove // later
+    private String email;
+}
