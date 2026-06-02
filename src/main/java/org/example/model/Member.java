88f9d29ae62445212641e8e5541c1cc2f22f2741
a-hlebowicz <aleksanderhlebowicz@gmail.com>
2026-06-03 00:49:38 +0200

    fix member name

--- src/main/java/org/example/model/Member.java
+++ src/main/java/org/example/model/Member.java
@@ -18,7 +18,7 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false)
